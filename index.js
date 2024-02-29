a197844ea9a5ab3cd442a41bff9ffd2a66389bf4
kanyaw <kanyawyousif243@gmail.com>
2024-02-29 19:00:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+customerName = 'bob';
+
+// Function to uppercase customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase(); 
+}
+
+// Function to set bestCustomer
+function setBestCustomer() {
+  bestCustomer = 'not bob'; 
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'; 
+}
+
+// Constant for leastFavoriteCustomer
+const leastFavoriteCustomer = 'someone';
+
+// Function to change leastFavoriteCustomer
+// Function to change leastFavoriteCustomer
+function changeLeastFavoriteCustomer(newLeastFavorite) {
+  throw new Error('Assignment to constant variable.');
+}
+
+
+
+// Export all variables and functions
+module.exports = { customerName, upperCaseCustomerName, setBestCustomer, overwriteBestCustomer, leastFavoriteCustomer, changeLeastFavoriteCustomer };
